2e3bb551b1dd0719698033a1052d2b825057631a
VagueFuture <igrokart97@mail.ru>
2020-08-28 20:03:46 +0700

    first

---
+++ Assets/Script/safe_minigame.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class safe_minigame : MonoBehaviour
+{
+    
+    [Header("Win Position")]
+    [Range(0,4)]
+    public List<int> winPos = new List<int>();
+
+    [Header("Deafult Position")]
+    [Range(0,4)]
+    public List<int> DeafultPos = new List<int>();
+    public List<shiftPin> pins = new List<shiftPin>();
+    public int stepCount = 5;
+    public GameObject safe;
+    public GameObject wallColor;
+    public float speedFade = 1.5f;
+    public AnimationCurve curve;
+    public AudioClip Close,Open;
+    private List<int> codInGame = new List<int>();
+    private bool complite = false;
+    private AudioSource audioS;
+
+    public void PressPin(int pinNum){
+        //Calculate new position for pin
+        codInGame[pinNum]++;
+        codInGame[pinNum] %= stepCount;
+        pins[pinNum].MovePin(codInGame[pinNum]);
+        if(CheckCompl()){
+            audioS.clip = Open;
+            audioS.Play();
+            StartCoroutine(FadeIn(false));
+            complite = true;
+        }
+        //Debug.Log(codInGame[pinNum]);  
+
+    }
+
+    public void OpenGame(){
+        //The function opens the game panel on the screen
+        // and sets the default values if game not complite
+        if(!complite){
+            codInGame.Clear();
+            gameObject.SetActive(true);
+
+            audioS = gameObject.GetComponent<AudioSource>();
+            audioS.clip = Close;
+            audioS.Play();
+
+            for(int i=0;i< pins.Count;i++){
+                pins[i].SetMyDeafultPosition(DeafultPos[i]);
+            }
+            for(int i=0;i<DeafultPos.Count;i++){
+                codInGame.Add(DeafultPos[i]);
+            }
+            StartCoroutine(FadeIn(true));
+        }
+    }
+
+    private bool CheckCompl(){
+        //checking the position of pins in victorious states
+        bool win = true;
+        for(int i=0;i<winPos.Count;i++){
+            if(winPos[i] != codInGame[i])
+                win = false;
+        }
+        return win;
+    }
+
+    IEnumerator FadeIn (bool hide)
+        {//smooth fade
+            float t = 1f;
+            while (t > 0f)
+            {
+                t -= Time.deltaTime* speedFade;
+                float a = curve.Evaluate(t);
+                if(!hide)
+                    a = curve.Evaluate(1-t);
+                wallColor.GetComponent<Image>().color = new Color(244,255,143,a);
+                yield return 0;
+            }
+            if(!hide){
+                gameObject.SetActive(false);
+                safe.SetActive(true);
+            }
+        }
+}
---
+++ Assets/Script/shiftPin.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shiftPin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Range(0,100)]
+    public int zeroPos = 92;
+    public int step = 44;
+    public int lift = 10;
+    public float speedAnim;
+    private Vector3 wVc;
+    private bool move,lift_back;
+    private AudioSource audioS;
+    void Start(){  
+
+        audioS = gameObject.GetComponent<AudioSource>();
+    
+    }
+    
+    private void FixedUpdate() {
+
+        //look for need animate
+        if(move)
+            Animate();
+
+    }
+    public void SetMyDeafultPosition(int pos){
+       
+       int newPos = zeroPos;
+       for(int i = 0;i<pos;i++){
+            newPos += step;
+        }
+        transform.localPosition = new Vector2(transform.localPosition.x,newPos);
+    
+    }
+
+    public void MovePin(int pos){
+        
+        // This function is called in the button and calculate new position
+        // this is the trigger to push
+        audioS.Play();
+        wVc = transform.localPosition;
+        wVc.y = zeroPos;
+        for(int i = 0;i<pos;i++){
+            wVc.y += step;
+        }
+        move = true;
+
+    }
+
+    //Animate pin if player press
+    private void Animate(){
+
+        Vector3 wVc_lift = new Vector3(wVc.x,wVc.y+lift,wVc.z);
+        if(wVc_lift!=transform.localPosition && !lift_back)
+
+            //Pin deflection by a specified distance
+            transform.localPosition = Vector2.MoveTowards(transform.localPosition,wVc_lift,Time.deltaTime*speedAnim);
+        
+        else{
+            
+            lift_back = true;
+            transform.localPosition = Vector2.MoveTowards(transform.localPosition,wVc,Time.deltaTime*speedAnim);
+            if(wVc == transform.localPosition){
+                move = false;
+                lift_back = false;
+            }
+        
+        }
+
+    }
+}
